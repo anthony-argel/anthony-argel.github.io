c2151179fdbf5d21a4114cc84a2a1630c88796b0
Anthony Argel <programmertuny@gmail.com>
2021-06-16 18:12:50 -0700

    made an attempt to keep fonts responsive

--- src/components/home.js
+++ src/components/home.js
@@ -51,13 +51,13 @@ function Home(props) {
         <div className='row'>
             <div className='col-12 col-lg-3 information d-flex flex-column justify-content-center'>
                 <div>
-                    <h1 className='display-4 fw-bold'>Anthony Argel</h1>
-                    <h4>Fullstack Developer</h4>
-                    <a href={resume} download className='h2' style={{textDecoration:'none'}}>Resume</a><br/>
+                    <h1 className='fw-bold' style={{fontSize:'max(2.4vw, 4vh)'}}>Anthony Argel</h1>
+                    <h4 style={{fontSize:'max(1.2vw, 3vh)'}}>Fullstack Developer</h4>
+                    <a href={resume} download className='h2' style={{textDecoration:'none', fontSize:'max(1.2vw, 3vh)'}}>Resume</a><br/>
                     <div className='d-flex justify-content-around mt-3'>
-                    <a href='mailto:anthony.r.argel@gmail.com'><AiOutlineMail color='black' size='1.7em'/></a>
-                    <a href='https://www.linkedin.com/in/anthony-argel-24735116a/'><FaGithub color='black' size='1.7em'/></a>
-                    <a href='https://github.com/anthony-argel'><FaLinkedin color='black' size='1.7em'/></a>
+                    <a href='mailto:anthony.r.argel@gmail.com'><AiOutlineMail color='black' size='max(1.7vw, 4vh)'/></a>
+                    <a href='https://www.linkedin.com/in/anthony-argel-24735116a/'><FaLinkedin color='black' size='max(1.7vw, 4vh)'/></a>
+                    <a href='https://github.com/anthony-argel'><FaGithub color='black' size='max(1.7vw, 4vh)'/></a>
                     </div>
                 </div>
             </div>
@@ -66,8 +66,8 @@ function Home(props) {
 
 
             <div className='col-12 col-lg-9 p-3 projects'>
-                    <h3 className='text-center display-4 mb-0'>Projects</h3>
-                    <hr/>
+                    <h3 className='text-center mb-0' style={{fontSize:'max(2.5vw, 4vh)'}}>Projects</h3>
+                    <hr style={{marginBottom:'min(10px, .5vh)', marginTop:'min(100px, .5vh)'}}/>
                 <div className='grid-container'>
 
                 <div className='grid-item item-1'>
@@ -75,21 +75,21 @@ function Home(props) {
                 </div>
 
                 <div className='grid-item item-2 mx-auto my-auto'>
-                <FaArrowAltCircleLeft size='2em'  cursor='pointer' onClick={() => nextProject(-1)}/>
+                <FaArrowAltCircleLeft cursor='pointer' size='max(1.7vw, 4vh)' onClick={() => nextProject(-1)}/>
                 </div>
 
                 <div className='grid-item item-3 mx-auto my-auto'>
-                <FaArrowAltCircleRight size='2em' cursor='pointer' onClick={() => nextProject(1)}/>
+                <FaArrowAltCircleRight cursor='pointer' size='max(1.7vw, 4vh)' onClick={() => nextProject(1)}/>
                 </div>
 
 
                 <div className='grid-item item-4'>
-                    <p className='fs-2 fw-bold'>{projects[currentProjectInd].title}</p>
-                    <p>{projects[currentProjectInd].description}</p>
+                    <p className='fw-bold' style={{fontSize:'max(1.3vw, 4vh)'}}>{projects[currentProjectInd].title}</p>
+                    <p style={{fontSize:'max(1.3vw, 2vh)'}}>{projects[currentProjectInd].description}</p>
                     <div className='d-flex justify-content-center'>
-                        <a href={projects[currentProjectInd].url} target='_blank' rel='noopener noreferrer' className='btn btn-secondary mx-3'>Visit Site</a>
-                        <a href={projects[currentProjectInd].backend} target='_blank' rel='noopener noreferrer'  className='btn btn-primary mx-3'>Backend code</a>
-                        <a href={projects[currentProjectInd].frontend} target='_blank' rel='noopener noreferrer'  className='btn btn-primary mx-3'>Frontend code</a>
+                        <a href={projects[currentProjectInd].url}  style={{fontSize:'max(1vw, 2vh)'}} target='_blank' rel='noopener noreferrer' className='btn btn-secondary mx-3'>Visit Site</a>
+                        <a href={projects[currentProjectInd].backend}  style={{fontSize:'max(1vw, 2vh)'}} target='_blank' rel='noopener noreferrer'  className='btn btn-primary mx-3'>Backend code</a>
+                        <a href={projects[currentProjectInd].frontend}  style={{fontSize:'max(1vw, 2vh)'}} target='_blank' rel='noopener noreferrer'  className='btn btn-primary mx-3'>Frontend code</a>
                     </div>
                 </div>
             </div>
