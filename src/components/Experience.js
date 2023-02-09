02e2391fbb9f41f141c783c9fb41dfa4b0fec27a
Anthony Argel <programmertuny@gmail.com>
2023-02-08 18:17:19 -0800

    Removed 1stle logo from experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,23 +6,19 @@ const Experience = () => {
             <h2 className="text-center mt-5 mb-3">Experience</h2>
             <div className="col-12 p-0 d-flex flex-columns justify-content-center">
                 <div className="text-center">
-                    <p className="p-3 m-0">
+                    <p className=" m-0"></p>
+                    <h4>
                         <a
                             href="https://www.1stle.com/"
                             target="_blank"
                             rel="noreferrer"
                         >
-                            {" "}
-                            <img
-                                src={firstLightLogo}
-                                alt="1st light logo"
-                                className="w-50 h-auto"
-                            />
+                            1st Light Energy
                         </a>
-                    </p>
-                    <h4>
-                        Software Developer <br /> May 2022 - Present
                     </h4>
+                    <h5>
+                        Software Developer <br /> May 2022 - Present
+                    </h5>
                     <h5 className="mt-3">Achievements</h5>
                     <ul className="text-start">
                         <li>
